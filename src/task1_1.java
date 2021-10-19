b8e4068a0e0e04ce33f5c6c8b179c555d5550b16
37529 <VadzimKuzmenka@mail.ru>
2021-10-19 23:20:16 +0300

    Initial commit

---
+++ src/task1_1.java
@@ -0,0 +1,10 @@
+public class task1_1 {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 3;
+            int c = 12;
+            int z = ((a - 3) * b / 2) + c;
+            System.out.println("при а =" + a + ", " + "b =" + b + ", " + "с=" + c + ", " + "результат функции z равен" + " " + z);
+        }
+    }
+
---
+++ src/task1_2.java
@@ -0,0 +1,13 @@
+public class task1_2 {
+        public static void main(String[] args) {
+            double a = 5;
+            double b = 3;
+            double c = 12;
+            int a3 = 3;
+            int b2 = -2;
+
+            double z = (b + Math.sqrt(b * b + 4 * a * c)) / (2 * a) - Math.pow(a, a3) * c + Math.pow(b, b2);
+            System.out.println("при а =" + "5" + ", " + "b =" + b + ", " + " при с=" + c + ", " + "результат выражения равен" + z);
+        }
+    }
+
